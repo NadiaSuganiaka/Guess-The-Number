162cc9208e44f5b5a36a5961b31ef33e12a6d895
NadiaSuganiaka <suhaniaka.nadia@gmail.com>
2026-08-12 22:36:53 +0300

    feat: Displaying all guess attempts and JS completion

--- guess_the_number.js
+++ guess_the_number.js
@@ -3,7 +3,7 @@ const chosenNumberBox = document.querySelector('#chosenNumberBox')//загада
 const inputNumber = document.querySelector('#inputNumber')
 const sendButton = document.querySelector('#sendButton')//надсилання інпута
 const attemptsBox = document.querySelector('#attemptsBox')//великий дів зі спробами
-const attemptsArr = document.querySelector('#attemptsArr')//перелік спроб
+const attemptsArr = document.querySelector('#attemptsArr')//перелік спроб!
 const attemptsPopup = document.querySelector('#attemptsPopup')//великий дів зі спробами popup
 const modal = document.querySelector('#modal')
 
@@ -21,6 +21,7 @@ let attempt = 0;
 const chosenNumberBoxHTML = `
     <h1 class="chosenNumber">${chosenNumber}</h1>
 `;
+
 
 // Функція для оновлення значення спроб на екрані
 function updateAttempts() {
@@ -45,13 +46,43 @@ sendButton.addEventListener('click', async () => {
         const modal = document.querySelector('dialog');
         modal.showModal()
     } else if (userGuess > chosenNumber) {
-        alert("Загадане число менше");
-        
+        const attemptsArrMoreHTML = `
+            <div class="attemptsArrElement">
+                <p>${userGuess}</p>
+                <svg class="arrow" style="transform: rotate(90deg);" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" width="30" height="30" shape-rendering="crispEdges">
+                    <rect x="1" y="4" width="8" height="1" fill="black" />
+
+                    <rect x="5" y="1" width="1" height="1" fill="black" />
+                    <rect x="6" y="2" width="1" height="1" fill="black" />
+                    <rect x="7" y="3" width="1" height="1" fill="black" />
+
+                    <rect x="7" y="5" width="1" height="1" fill="black" />
+                    <rect x="6" y="6" width="1" height="1" fill="black" />
+                    <rect x="5" y="7" width="1" height="1" fill="black" />
+                </svg>
+            </div>
+        `;
+        attemptsArr.insertAdjacentHTML('afterbegin', attemptsArrMoreHTML);
     } else {
-        alert("Загадане число більше");
+        const attemptsArrMoreHTML = `
+            <div class="attemptsArrElement">
+                <p>${userGuess}</p>
+                <svg class="arrow" style="transform: rotate(270deg);" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" width="30" height="30" shape-rendering="crispEdges">
+                    <rect x="1" y="4" width="8" height="1" fill="black" />
+
+                    <rect x="5" y="1" width="1" height="1" fill="black" />
+                    <rect x="6" y="2" width="1" height="1" fill="black" />
+                    <rect x="7" y="3" width="1" height="1" fill="black" />
+
+                    <rect x="7" y="5" width="1" height="1" fill="black" />
+                    <rect x="6" y="6" width="1" height="1" fill="black" />
+                    <rect x="5" y="7" width="1" height="1" fill="black" />
+                </svg>
+            </div>
+        `;
+        attemptsArr.insertAdjacentHTML('afterbegin', attemptsArrMoreHTML);
     }
     inputNumber.value = "";
-});//chosenNumberBox.insertAdjacentHTML('afterbegin', chosenNumberBoxHTML);
-
+});
 
 chosenNumberBox.insertAdjacentHTML('afterbegin', chosenNumberBoxHTML);
